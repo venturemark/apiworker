778dc49685b6166060a96c20ce47ce831cb11602
Thomas Fussell <thomas@fussell.io>
2021-07-18 11:09:00 -0400

    Update email data for new template (#103)
    * update email data
    
    * sort updates and fix json encoding

--- pkg/handler/remindercreate/types.go
+++ pkg/handler/remindercreate/types.go
@@ -1,6 +1,22 @@
 package remindercreate
 
 type templateVenture struct {
-	Name string `json:"name"`
-	Slug string `json:"slug"`
+	Name string `json:"name,omitempty"`
+	Path string `json:"path,omitempty"`
+}
+
+type templateTimeline struct {
+	Name string `json:"name,omitempty"`
+	Path string `json:"path,omitempty"`
+}
+
+type templateUpdate struct {
+	IDNumeric    int64              `json:"-"`
+	Title        string             `json:"title,omitempty"`
+	Body         string             `json:"body,omitempty"`
+	AuthorName   string             `json:"authorName,omitempty"`
+	RelativeTime string             `json:"relativeTime,omitempty"`
+	Path         string             `json:"path,omitempty"`
+	Venture      templateVenture    `json:"venture"`
+	Timelines    []templateTimeline `json:"timelines,omitempty"`
 }
